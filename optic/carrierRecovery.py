4d54953d30e08af75b5e98c21196927e2b68b06c
valmir <valmir@goclever.io>
2023-01-05 00:24:16 -0300

    wip: vectorizing BPS algorithm

--- optic/carrierRecovery.py
+++ optic/carrierRecovery.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from numba import njit
+from numba import njit, cuda
 from numpy.fft import fft, fftfreq, fftshift
 
 from optic.dsp import pnorm
 from optic.modulation import GrayMapping
+
+import cupy as cp
+import yappi
+
+yappi.set_clock_type("wall")
 
 
 def cpr(Ei, symbTx=[], paramCPR=[]):
@@ -19,16 +24,16 @@ def cpr(Ei, symbTx=[], paramCPR=[]):
         Transmitted symbol sequence. The default is [].
     paramCPR : core.param object, optional
         configuration parameters. The default is [].
-        
+
         BPS params:
-            
+
         paramCPR.alg: CPR algorithm to be used ['bps' or 'ddpll']
         paramCPR.M: constellation order. The default is 4.
         paramCPR.N: length of BPS the moving average window. The default is 35.    
         paramCPR.B: number of BPS test phases. The default is 64.
-        
+
         DDPLL params:
-            
+
         paramCPR.tau1: DDPLL loop filter param. 1. The default is 1/2*pi*10e6.
         paramCPR.tau2: DDPLL loop filter param. 2. The default is 1/2*pi*10e6.
         paramCPR.Kv: DDPLL loop filter gain. The default is 0.1.
@@ -52,7 +57,7 @@ def cpr(Ei, symbTx=[], paramCPR=[]):
     # check input parameters
     alg = getattr(paramCPR, "alg", "bps")
     M = getattr(paramCPR, "M", 4)
-    constType = getattr(paramCPR, 'constType','qam')
+    constType = getattr(paramCPR, 'constType', 'qam')
     B = getattr(paramCPR, "B", 64)
     N = getattr(paramCPR, "N", 35)
     Kv = getattr(paramCPR, "Kv", 0.1)
@@ -69,15 +74,17 @@ def cpr(Ei, symbTx=[], paramCPR=[]):
     # constellation parameters
     constSymb = GrayMapping(M, constType)
     constSymb = pnorm(constSymb)
-    
+
     # 4th power frequency offset estimation/compensation
     Ei, _ = fourthPowerFOE(Ei, 1/Ts)
     Ei = pnorm(Ei)
-    
+
     if alg == "ddpll":
         θ = ddpll(Ei, Ts, Kv, tau1, tau2, constSymb, symbTx, pilotInd)
     elif alg == "bps":
         θ = bps(Ei, N // 2, constSymb, B)
+    elif alg == "bpsGPU":
+        θ = bpsGPU(Ei, N // 2, constSymb, B)
     else:
         raise ValueError("CPR algorithm incorrectly specified.")
     θ = np.unwrap(4 * θ, axis=0) / 4
@@ -90,7 +97,7 @@ def cpr(Ei, symbTx=[], paramCPR=[]):
     return Eo, θ
 
 
-@njit
+# @njit
 def bps(Ei, N, constSymb, B):
     """
     Blind phase search (BPS) algorithm
@@ -132,13 +139,176 @@ def bps(Ei, N, constSymb, B):
 
         for k in range(L):
             for indPhase, ϕ in enumerate(ϕ_test):
-                dist[indPhase, :] = np.abs(x[k, n] * np.exp(1j * ϕ) - constSymb) ** 2
+                # For each test phase, compute the distances to all constellation symbols
+                dist[indPhase, :] = np.abs(
+                    x[k, n] * np.exp(1j * ϕ) - constSymb) ** 2
+
+                # Save the minimum distance for the test phase
                 dmin[indPhase, -1] = np.min(dist[indPhase, :])
             if k >= 2 * N:
                 sumDmin = np.sum(dmin, axis=1)
                 indRot = np.argmin(sumDmin)
                 θ[k - 2 * N, n] = ϕ_test[indRot]
             dmin = np.roll(dmin, -1)
+    return θ
+
+
+@njit
+def gen_test_phases(num_rotations: int) -> np.ndarray:
+    return np.arange(num_rotations) * (np.pi / 2) / num_rotations
+
+
+# @njit
+def min_dist_to_constellation_symbols(symbols, const_symb):
+    new_shape = (len(const_symb), len(symbols))
+    resized_symbols = np.resize(symbols, new_shape).transpose()
+
+    dist_to_each_const_symbol = np.abs(resized_symbols - const_symb) ** 2
+    return dist_to_each_const_symbol.min(axis=1)
+
+
+# @njit
+def get_optimum_phase_angle(ϕ_test, dmin):
+    """
+    The optimum phase angle is determined by searching the minimum sum of
+    distance values.
+    """
+    sumDmin = np.sum(dmin, axis=1)
+    optimum_phase_index = np.argmin(sumDmin)
+    return ϕ_test[optimum_phase_index]
+
+
+# @njit
+def bpsGPU2(Ei, N, constSymb, B, prec=cp.complex128):
+    nModes = Ei.shape[1]
+
+    ϕ_test = gen_test_phases(B)
+
+    θ = np.zeros(Ei.shape, dtype="float")
+
+    zeroPad = np.zeros((N, nModes), dtype="complex")
+    x = np.concatenate(
+        (zeroPad, Ei, zeroPad)
+    )  # pad start and end of the signal with zeros
+
+    num_symbols_after_padding = x.shape[0]
+
+    # start = cuda.grid(1)
+
+    for mode_index in range(nModes):
+
+        # Minimum distance for each test phase
+        dmin = np.zeros((B, 2 * N + 1), dtype="float")
+
+        for symbol_index in range(num_symbols_after_padding):
+            symbol = x[symbol_index, mode_index]
+            symbol_rotations = symbol * np.exp(1j * ϕ_test)
+
+            dmin = min_dist_to_constellation_symbols(
+                symbol_rotations, constSymb)
+            # for indPhase, ϕ in enumerate(ϕ_test):
+            #     rotated_symbol = symbol * np.exp(1j * ϕ)
+            #     dmin[indPhase, -1] = min_dist_to_constellation_symbols(
+            #         rotated_symbol, constSymb)
+            if symbol_index >= 2 * N:
+                θ[symbol_index - 2 * N,
+                    mode_index] = get_optimum_phase_angle(ϕ_test, dmin)
+            dmin = np.roll(dmin, -1)
+
+
+def bpsDistances(x, ϕ_test, constSymb):
+    x_gpu = cp.asarray(x)
+    ϕ_test_gpu = cp.asarray(ϕ_test)
+    constSymb_gpu = cp.asarray(constSymb)
+
+    x_expanded = x_gpu[:, :, cp.newaxis]
+    ϕ_expanded = cp.exp(1j * ϕ_test_gpu)[None, None, :]
+    rotated_x = x_expanded * ϕ_expanded
+    constSymb_expanded = constSymb_gpu[None, None, None, :]
+    dist = cp.absolute(cp.subtract(
+        rotated_x[:, :, :, None], constSymb_expanded)) ** 2
+
+    return cp.asnumpy(dist)
+
+
+def bpsDistancesNumpy(x, ϕ_test, constSymb):
+    x_expanded = x[:, :, np.newaxis]
+    ϕ_expanded = np.exp(1j * ϕ_test)[None, None, :]
+    rotated_x = x_expanded * ϕ_expanded
+    constSymb_expanded = constSymb[None, None, None, :]
+    dist = np.abs(np.subtract(
+        rotated_x[:, :, :, None], constSymb_expanded)) ** 2
+
+    return dist
+
+
+def bpsGPU(Ei, N, constSymb, B):
+    yappi.start()
+    # Ei = Ei[:5]
+    nModes = Ei.shape[1]
+
+    ϕ_test = np.arange(0, B) * (np.pi / 2) / B  # test phases
+
+    θ = np.zeros(Ei.shape, dtype="float")
+
+    zeroPad = np.zeros((N, nModes), dtype="complex")
+    x = np.concatenate(
+        (zeroPad, Ei, zeroPad)
+    )  # pad start and end of the signal with zeros
+
+    L = x.shape[0]
+    dmin = np.zeros((B, 2 * N + 1), dtype="float")
+
+    # x.shape = (100084,2)
+    # ϕ_test.shape = (64,)
+    # rotated_x.shape = (100084, 2, 64) -> The element rotated_x[500][0] has the 64 rotations of input x[500][0]
+    # dist.shape = (100084, 2, 64, 16)
+
+    dist_cupy = bpsDistances(x, ϕ_test, constSymb)
+    # dist = bpsDistancesNumpy(x, ϕ_test, constSymb)
+    # assert np.array_equiv(dist.round(decimals=5), dist_cupy.round(decimals=5))
+
+    yappi.get_func_stats().print_all()
+    yappi.get_func_stats().save("ystats1.ys")
+
+    import sys
+    sys.exit(1)
+
+    dmin = np.roll(dmin, -1, axis=-1)
+    sumDmin = np.sum(dmin, axis=1)
+    indRot = np.argmin(sumDmin, axis=-1)
+    θ = ϕ_test[indRot]
+
+    return θ
+
+
+def bpsGPU3(Ei, N, constSymb, B):
+    Ei = Ei[:100]
+    nModes = Ei.shape[1]
+
+    ϕ_test = np.arange(0, B) * (np.pi / 2) / B  # test phases
+
+    zeroPad = np.zeros((N, nModes), dtype="complex")
+    x = np.concatenate(
+        (zeroPad, Ei, zeroPad)
+    )  # pad start and end of the signal with zeros
+
+    θ = np.zeros(x.shape, dtype="float")
+    dmin = np.zeros((B, 2 * N + 1), dtype="float")
+
+    x_expanded = x[:, :, np.newaxis]
+    ϕ_expanded = np.exp(1j * ϕ_test)[None, None, :]
+    rotated_x = x_expanded * ϕ_expanded
+    constSymb_expanded = constSymb[None, None, None, :]
+    dist = np.abs(np.subtract(
+        rotated_x[:, :, :, None], constSymb_expanded)) ** 2
+    dist = np.squeeze(dist)  # shape: (100084, 2, 64)
+    dmin[:, -1] = np.min(dist, axis=-1)
+    dmin = np.roll(dmin, -1, axis=-1)
+    sumDmin = np.sum(dmin, axis=1)
+    indRot = np.argmin(sumDmin, axis=-1)
+    θ[N:-N] = ϕ_test[indRot]  # shape: (100084,)
+
     return θ
 
 
@@ -175,7 +345,7 @@ def ddpll(Ei, Ts, Kv, tau1, tau2, constSymb, symbTx, pilotInd):
     -------
     [1] H. Meyer, Digital Communication Receivers: Synchronization, Channel 
     estimation, and Signal Processing, Wiley 1998. Section 5.8 and 5.9.    
-    
+
     """
     nModes = Ei.shape[1]
 
