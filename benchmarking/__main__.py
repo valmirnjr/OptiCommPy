f43f7bf9cf10943d2e1ed6e55f68fd662abddd9b
valmir <valmir@goclever.io>
2023-01-09 22:45:04 -0300

    update: try catches assertion BPS result

--- benchmarking/__main__.py
+++ benchmarking/__main__.py
@@ -9,23 +9,28 @@ import optic.carrierRecovery as carrierRecovery
 
 
 def assert_bps_result(θ):
-    _, expected_θ = common.get_expected_BPS_results()
-    np.testing.assert_allclose(θ, expected_θ)
+    try:
+        _, expected_θ = common.get_expected_BPS_results()
+        np.testing.assert_allclose(θ, expected_θ)
+        print("Assertion of result passed!")
+    except AssertionError as err:
+        print(f"Assertion error: {err}")
 
 
 def main():
     alg = "bps" if len(sys.argv) <= 1 else sys.argv[1]
     loops = 0 if len(sys.argv) <= 2 else int(sys.argv[2])
 
+    print(f"Running carrier recovery algorithm '{alg}'.")
+
     sigRx = common.getSigRx()
     paramCPR = common.get_paramCPR(alg)
 
     y_CPR, θ = carrierRecovery.cpr(sigRx, paramCPR=paramCPR)
 
-    # print(cp.cuda.get_nvcc_path())
-    # for _ in range(loops):
-    #     os.system("echo loop = " + str(_))
-    #     y_CPR, θ = carrierRecovery.cpr(sigRx, paramCPR=paramCPR)
+    for _ in range(loops):
+        y_CPR, θ = carrierRecovery.cpr(sigRx, paramCPR=paramCPR)
+
     assert_bps_result(θ)
 
 
